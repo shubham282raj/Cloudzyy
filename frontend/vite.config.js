8bcac0e2070f62106d634a20fdf91f94271762b7
Shubham <shubham282raj@gmail.com>
2025-04-04 21:01:06 +0530

    PWA Support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     port: 4011,
   },
 });
+
+const manifestForPlugIn = {
+  registerType: "autoUpdate",
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true,
+    globPatterns: ["**/*.{svg,ico}"],
+    navigateFallback: "/index.html",
+  },
+  // devOptions: {
+  //   enabled: true,
+  // },
+  includeAssets: ["favicon.svg", "**/*"],
+  navigateFallback: "/index.html",
+  manifest: {
+    name: "Cloudzyy",
+    short_name: "Cloudzyy",
+    description: "Cloudzyy - A Github Repo Container",
+    icons: [
+      {
+        src: "favicon.svg",
+        sizes: "any",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#111827",
+    background_color: "#111827",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+  },
+};
